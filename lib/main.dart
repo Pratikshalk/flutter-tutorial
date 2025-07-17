6c4ba1c85aa0327642ee2feaab76ae2f7b809748
Pratikshalk <pratikshakhemnar164@gmail.com>
2025-07-17 11:56:05 +0530

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:login_app/pages/home_page.dart';
 
 void main(){
   runApp(MyApp());
@@ -7,16 +10,19 @@ void main(){
 class MyApp extends StatelessWidget{
   const MyApp({Key? key}): super(key: key);
 
-
+  @override
   Widget build(BuildContext context){
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to Flutter Tutorial Pratiksha"),
-          ),
-        ),
+      
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        brightness: Brightness.light
       ),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.red
+      ),
+      home: const HomePage(),
     );
   }
 }
